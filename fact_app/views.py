35df222111109018ab4114eb1e89fb2b98c6ec3f
mariam <mariamlengane02@gmail.com>
2023-03-16 16:58:45 +0000

    feat: models definition

--- fact_app/views.py
+++ fact_app/views.py
@@ -1,7 +1,6 @@
-
 from django.shortcuts import render
 from django.views import View
-from . models import *
+from .models import *
 
 # Create your views here.
 
@@ -23,6 +22,21 @@ class HomeView(View):
     def post(self, request, *args, **kwagrs):
 
         return render(request, self.templates_name, self.context) 
+
+ 
+class AddCustomerView(View):
+    """ add new customer """    
+    template_name = 'add_customer.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwagrs):
+        print(request.POST)
+        return render(request, self.template_name)
+
+
+
     
 
 
