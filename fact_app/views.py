8b339a6353fb21300ada244ce248c4f02ae37efe
71913382 <marismalingani@gmail.com>
2023-03-16 13:27:04 +0000

    feld: model definitions

--- fact_app/views.py
+++ fact_app/views.py
@@ -1,4 +1,4 @@
-from tkinter.tix import Select
+
 from django.shortcuts import render
 from django.views import View
 from . models import *
@@ -25,29 +25,7 @@ class HomeView(View):
         return render(request, self.templates_name, self.context) 
     
 
-    class AddCustomerView(View):
-        """Add new customer"""
 
-        template_name = 'add_customer.html'
-
-        def get(self, request, *args, **kwags):
-         return render(request, self.template_name)   
-        
-        def post(self, request, *args, **kwagrs): 
-         data= {
-            'name': request.POST.get('name'),
-            'email': request.POST.get('email'),
-            'phone':request.POST.get('phone'),
-            'address':request.POST.get('address'),
-            'sex':request.POST.get('sex'),
-            'age':request.POST.get('age'),
-            'city':request.POST.get('city'),
-            'zip':request.POST.get('zip'),
-            'save_by':request.user
-            
-         }
-           
-         return render(request, self.template_name)
 
 
     
